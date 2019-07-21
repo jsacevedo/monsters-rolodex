53115d3720895b4ac56799d33fa8b7dec64f15ea
jaceweb <jsacevedo@jacewebdesign.com>
2019-07-21 18:31:45 -0500

    Updates

---
+++ precache-manifest.9de7738e82abaa9ca441c90000ac0201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a3cba285c8077703ad482071a64597",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d70bc2b4f2efc8720df6",
+    "url": "/monsters-rolodex/static/css/main.5251240d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d70bc2b4f2efc8720df6",
+    "url": "/monsters-rolodex/static/js/main.db779b4b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
